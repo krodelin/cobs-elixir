5cf4864e2b9b2db0ab9636c8c61561b0bf9488b4
Udo Schneider <Udo.Schneider@homeaddress.de>
2018-02-20 16:04:20 +0100

    Bumped version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cobs.Mixfile do
   def project do
     [
       app: :cobs,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
