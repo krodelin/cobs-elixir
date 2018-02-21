1ca6fd90625a10a777934c6e6c93ff6c4831db84
Udo Schneider <Udo.Schneider@homeaddress.de>
2018-02-21 14:44:52 +0100

    Added tuple returns

--- lib/cobs.ex
+++ lib/cobs.ex
@@ -8,27 +8,27 @@ defmodule Cobs do
 
   ## Example
       iex> Cobs.encode(<<0x01, 0x02, 0x00, 0x03>>)
-      <<0x03, 0x01, 0x02, 0x02, 0x03>>
+      {:ok, <<0x03, 0x01, 0x02, 0x02, 0x03>>}
   """
 
-  @spec encode(binary()) :: binary()
+  @spec encode(binary()) :: {:ok, binary()} | {:error | any()}
   def encode(binary) do
-    do_encode(<<>>, binary)
+    if byte_size(binary) <= 254 do
+      {:ok, do_encode(<<>>, <<>>, binary)}
+    else
+      {:error, nil}
+    end
   end
 
-  defp do_encode(head, <<>>) do
-    ohb = byte_size(head) + 1
-    <<ohb>> <> head
-  end
+  @spec do_encode(binary(), binary(), binary()) :: binary()
+  defp do_encode(head, block, tail)
 
-  defp do_encode(head, <<0, tail :: binary>>) do
-    ohb = byte_size(head) + 1
-    <<ohb>> <> head <> do_encode(<<>>, tail)
-  end
-
-  defp do_encode(head, <<val, tail :: binary>>) do
-    do_encode(head <> <<val>>, tail)
-  end
+  defp do_encode(head, block, <<>>),
+       do: head <> <<byte_size(block) + 1>> <> block
+  defp do_encode(head, block, <<0, tail :: binary>>),
+       do: do_encode(head <> <<byte_size(block) + 1>> <> block, <<>>, tail)
+  defp do_encode(head, block, <<val, tail :: binary>>),
+       do: do_encode(head, block <> <<val>>, tail)
 
   @doc """
   Decode COBS encoded binary (without `0` bytes) into a binary (with `0` bytes).
@@ -36,18 +36,31 @@ defmodule Cobs do
 
   ## Example
       iex> Cobs.decode(<<0x03, 0x01, 0x02, 0x02, 0x03>>)
-      <<0x01, 0x02, 0x00, 0x03>>
+      {:ok, <<0x01, 0x02, 0x00, 0x03>>}
   """
-  @spec decode(binary()) :: binary()
+  @spec decode(binary()) :: {:ok, binary()} | {:error | any()}
   def decode(binary)
 
-  def decode(<<>>) do
-    <<>>
+  def decode(binary) do
+    do_decode(<<>>, binary)
   end
 
-  def decode(<<ohb, tail :: binary>>) do
-    head_length = ohb - 1
-    <<block :: binary - size(head_length), remaining :: binary>> = tail
-    if byte_size(remaining) > 0, do: block <> <<0>> <> decode(remaining), else: block
+  @spec do_decode(binary(), binary()) :: {:ok, binary()} | {:error | any()}
+  defp do_decode(head, tail)
+
+  defp do_decode(head, <<>>) do
+    {:ok, head}
   end
+
+  defp do_decode(head, <<ohb, tail :: binary>>) do
+    block_length = ohb - 1
+    if block_length > byte_size(tail) do
+      {:error, nil}
+    else
+      <<block :: binary - size(block_length), remaining :: binary>> = tail
+      new_head = if byte_size(remaining) > 0, do: head <> block <> <<0>>, else: head <> block
+      do_decode(new_head, remaining)
+    end
+  end
+
 end
